05babe723e5065692a6b88e8601f4a92139d09fb
aleksey05 <aleksey05sk@bigmir.net>
2016-03-30 10:13:39 +0300

    angular validation added

--- MoneyClient/MoneyControllers.js
+++ MoneyClient/MoneyControllers.js
@@ -1,15 +1,29 @@
-var money = angular.module('money',[]);
+var money = angular.module('money',['jcs-autoValidate']);
+
+/*money.run(function(defaultErrorMesssageResolver){
+defaultErrorMesssageResolver.getErrorMessages().then(function(errorMessages){
+   errorMessages['wrongSimbolsInName'] = 'You shoul use only letters, digits and underscore simbol only';
+ });
+});*/
 
 money.controller('moneyCtrl', function($scope, $http){
-$http.get('http://localhost:8080/Money/adv/list').success(function(data) {
-    $scope.advertisements = data; 
+$http.get('http://localhost:8080/Money/adv/list').success(function(responce) {
+    $scope.advertisements = responce; 
    });
 
-    $scope.advertisement={};
+     $scope.advertisement={};
+     
+
+     $scope.edit = function(adv){
+     	console.log(adv);
+      $scope.advertisement=adv;
+        console.log($scope.advertisement);
+      }    
 
     $scope.add = function(){
     $http.post('http://localhost:8080/Money/adv/add', $scope.advertisement).
             success(function(responce){
+            	$scope.advertisement={};
       	      console.log('ok')
      }).error(function(responce){
               console.log("error")
